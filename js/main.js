4abbab88360541e46f55a30648cd1a428c611f0c
Brandon Olin <brandon@devblackops.io>
2016-02-20 16:02:22 -0800

    Fix mobile menu

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ layout: null
 
 var host = "devblackops.io";
 if ((host == window.location.host) && (window.location.protocol != "https:"))
-    window.location.protocol = "https";
+  window.location.protocol = "https";
 
 $(document).ready(function () {
   // $('a.blog-button').click(function (e) {
@@ -19,13 +19,13 @@ $(document).ready(function () {
   //   }
   // })
 
-  //#if (window.location.hash && window.location.hash == '#blog') {
+  // #if (window.location.hash && window.location.hash == '#blog') {
   //  $('.panel-cover').addClass('panel-cover--collapsed')
-  //}
-
-  //if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
+  // }
+  //
+  // if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
   //  $('.panel-cover').addClass('panel-cover--collapsed')
-  //}
+  // }
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
@@ -36,5 +36,4 @@ $(document).ready(function () {
   //   $('.navigation-wrapper').toggleClass('visible')
   //   $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   // })
-
 })
