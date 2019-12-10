b75e7f93b8f0679cf7d8292a6c13ba5942ac360b
Brandon Olin <brandon@devblackops.io>
2019-12-10 00:21:48 -0800

    Add jekyll container for local dev

---
+++ build.ps1
@@ -0,0 +1,1 @@
+docker build -t devblackops.io $PWD
---
+++ run.ps1
@@ -0,0 +1,3 @@
+# With fixes for Windows, docker, and 0.0.0.0
+# https://tonyho.net/jekyll-docker-windows-and-0-0-0-0/
+docker run -it -p 4000:4000 -e "JEKYLL_ENV=docker" -v "$PWD`:/srv/jekyll" devblackops.io jekyll serve --watch --force_polling --incremental --strict_front_matter --drafts --future --config _config.yml,_config_docker.yml
